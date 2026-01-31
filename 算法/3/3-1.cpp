7831ae97db6ad367c13110554d14dd8a1d7a7f80
Cq-pro <139827608+Cq-pro@users.noreply.github.com>
2026-01-31 21:48:30 +0800

    Add files via upload

---
+++ 算法/3/3-1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxPathSum(vector<vector<int>>& triangle, int n) {
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    // 将三角形最后一行的值复制到 dp 数组中
+    for (int i = 0; i < n; ++i) {
+        dp[n - 1][i] = triangle[n - 1][i];
+    }
+
+    // 从倒数第二行开始自底向上计算最大路径和
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = 0; j <= i; ++j) {
+            dp[i][j] = triangle[i][j] + max(dp[i + 1][j], dp[i + 1][j + 1]);
+        }
+    }
+
+    return dp[0][0]; // 返回顶部的最大路径和
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> triangle(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cin >> triangle[i][j];
+        }
+    }
+
+    cout << maxPathSum(triangle, n) << endl;
+
+    return 0;
+}
---
+++ 算法/3/3-2.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+const int MAX_N = 201;
+const int INF = 0x7fffff;
+int n;
+int f[MAX_N][MAX_N];
+
+void dp() {
+
+    for (int i = 1; i <= n; i++) {
+        f[i][i] = 0;
+    }
+
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            int v = f[i][j];
+            for (int k = i + 1; k <= j - 1; k++) {
+                v = min(v, f[i][k] + f[k][j]);
+                // cout << "update f = " << v << endl;
+            }
+            f[i][j] = v;
+        }
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> n;
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            cin >> f[i][j];
+        }
+    }
+    dp();
+    cout << f[1][n] << endl;
+}
